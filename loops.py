b88703b676acb1b58e7ef02a6a62e1d7fd734e65
whydoesitmatter <samllarochelle@gmail.com>
2020-07-14 11:50:34 -0400

    loops.py
    updated to add continue and nested loops

--- loops.py
+++ loops.py
@@ -38,6 +38,8 @@ def main():
   # This for loop also automatically increments at the end of each iteration.
   # However, it increments val by one index in the list.
 
+  print("")
+
   # Another way to increment through a list is using the range function.
 
   for z in range(0, len(nums)):   # Create variable z at index 0
@@ -45,9 +47,20 @@ def main():
 
   # It will repeat as many times as there are values in nums. (len = length)
 
-  print("\n_______FOR... ELSE_______")
+  print("\n__________ELSE___________")
 
-  # The for loop can include an "else" block.
+  # The while loop can include an "else" block.
+  # The "else" will execute when the statement becomes false.
+
+  x = 0
+  while (x < 7):
+    print(x)
+    x += 1
+  else:
+    print("x is now equal to 7 \n")
+  
+  # The for loop can also include an "else" block.
+  # The "else" will execute when it is done iterating.
 
   names = ["salty", "kuma", "witchy"]
   for i in names:
@@ -65,5 +78,44 @@ def main():
       break
   else:                       # If no name in list equals sample name
     print("No such name exists in the database. \n")
+
+  print("\n________CONTINUE_________")
+
+  # When a "continue" statement is hit, you will return to the top of the loop.
+  # Here is an example of a "continue" statement in a while loop, comparable to a do while loop.
+  # Do while loops execute once before checking the statement.
+  # Here, we increment once before checking if y is divisible by 2.
+
+  y = -1                # The increment exists at the beginning
+  while (y < 7):
+    y += 1              # This has to come before the "if" or it will loop forever
+    if (y % 2 != 0):    # If y is not divisible by 2, the below statement will be executed
+      continue        # This statement will skip anything AFTER it
+    print(y)
+  
+  print("")
+
+  # Here is an example of a "continue" statement in a for loop.
+
+  for letter in "Pythons":
+    if letter == "s":       # Skip the letter "s"
+      continue
+    print(letter)
+
+  print("\n______NESTED LOOPS_______")
+
+  # You can also nest a loop inside another loop.
+
+  list = [("salty", "d.va", "mercy"), ("kuma", "torbjorn", "reinhardt"), ("witchy", "moira", "junkrat")]
+
+  for r in range(0, 3):       # For row, there are 3 values per row (0-2)
+    for c in range(0, 3):     # For column, there are 3 values per column (0-2)
+      print(list[r][c])       # Print data at every loop
+    print("")                 # Create a break between rows
+
+  # This should follow the pattern:
+  # [0][0], [0][1], [0][2]
+  # [1][0], [1][1], [1][2]
+  # [2][0], [2][1], [2][2]
 
 main()
